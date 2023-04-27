9ba31c12271e9aa35e7df4abf829674685313bc9
Giulio de Cadilhac <g.decadilhac@gmail.com>
2023-04-27 19:43:42 +0200

    changes described in push

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,35 +1,43 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
   def index
-    @projects = Project.all
+    @blog_posts = Blog_post.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
-    @project = Project.new
+    @blog_post = Blog_post.new
   end
 
   def create
-    @project = Project.new(project_params)
-    @project.save
-    redirect_to project_path(@project)
+    @blog_post = Blog_post.new(blog_post_params)
+    @blog_post.save
+    redirect_to blog_post_path(@blog_post)
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
-    @project.update(project_params)
-    redirect_to project_path(@project)
+    @blog_post.update(blog_post_params)
+    redirect_to blog_post_path(@blog_post)
   end
 
   def destroy
-    @project = Project.find(params[:id])
-    @project.destroy
-    redirect_to projects_path
+    @blog_post.destroy
+    redirect_to blog_posts_path
+  end
+
+  private
+
+  def set_blog_post
+    @blog_post = Blog_post.find(params[:id])
+  end
+
+  def blog_post_params # strong params
+    params.require(:blog_post).permit(:likes)
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,11 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -18,19 +19,25 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     @project.update(project_params)
     redirect_to project_path(@project)
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     redirect_to projects_path
   end
 
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params # strong params
+    params.require(:project).permit(:project_title, :project_description, :company_name, :company_description, :completion_date, :sector, :service, :website, :instagram, :linkedin, :tiktok, :facebook)
+  end
 end
