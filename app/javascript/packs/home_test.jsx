3bc861ecb1452324b72ad7ab6d9277f4ecf8bfa5
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-04-27 12:23:48 +0100

    creates test home.jsx and renders to home.html.erb

---
+++ app/javascript/packs/home_test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const home = (
+  <div>
+    <p>hihihihih</p>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(home);
